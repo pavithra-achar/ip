debd0bb0f6f4a1b90ede05d5b8632261e549b11b
Pavithra Achar <pavithra.achar1505@gmail.com>
2026-01-20 15:41:05 +0800

    Convert the if-statements to a switch-case

--- src/main/java/Verse.java
+++ src/main/java/Verse.java
@@ -11,7 +11,7 @@ public class Verse {
         System.out.println(greeting);
 
         boolean proceed = true;
-        ArrayList<String> list = new ArrayList<>(100);
+        ArrayList<Task> list = new ArrayList<>(100);
 
         Scanner sc = new Scanner(System.in);
 
@@ -19,24 +19,29 @@ public class Verse {
             System.out.print("User : ");
             String sentence = sc.nextLine();
 
-            if (sentence.equals("bye")) {
-                proceed = false;
-            } else if (sentence.equals("list")) {
-                System.out.println("Verse : Here lies all that is noted:");
+            switch (sentence) {
+                case "bye" :
+                    proceed = false;
+                    break;
+                case "list" :
+                    System.out.println("Verse : Here lies all that is noted:");
 
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i + 1) + ". " + list.get(i));
-                }
-            } else {
-                list.add(sentence);
-                System.out.println("Verse : " + sentence + " hath been added to list.");
+                    for (int i = 0; i < list.size(); i++) {
+                        Task t = list.get(i);
+                        System.out.println((i + 1) + ". ["+ t.getStatusIcon() +" ] " + t);
+                    }
+                    break;
+                default :
+                    Task t = new Task(sentence);
+                    list.add(t);
+                    System.out.println("Verse : " + sentence + " hath been added to list.");
             }
         }
 
         sc.close();
 
         System.out.println();
-        System.out.println("ACT I - Scene 2");
+        System.out.println("Final Act");
         System.out.println(farewell);
     }
 }
