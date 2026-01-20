2fe9f955de76e94defaeb840249b88123fcddf77
Pavithra Achar <pavithra.achar1505@gmail.com>
2026-01-20 12:50:08 +0800

    Add and display greeting and farewell message

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Verse User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Verse.java
@@ -0,0 +1,12 @@
+public class Verse {
+    public static void main(String[] args) {
+        String greeting = "Good day user, I am Verse. Thy words await my wit.\n" +
+                "Speak, and declare thy query.\n";
+        String farewell = "Till next thou call’st this system forth, farewell.\n";
+
+        System.out.println("ACT I - Scene 1");
+        System.out.println(greeting + "\n");
+        System.out.println("ACT I - Scene 2");
+        System.out.println(farewell);
+    }
+}
